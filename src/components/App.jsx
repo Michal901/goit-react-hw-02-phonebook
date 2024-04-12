eeaf1372977d45084d5401fb986cf80478358095
Michal901 <momot1122@wp.pl>
2024-04-12 22:02:02 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,73 +1,71 @@
 import { useState } from 'react';
 
 export function App() {
-  const [review, setReview] = useState(null);
-  const [inputValue, setInputValue] = useState('');
-  const [textareaValue, setTextareaValue] = useState('');
+  const [contactBook, setContactBook] = useState({
+    contacts: [],
+    name: '',
+  });
+
+  const { contacts, name } = contactBook;
 
   const handleSubmit = e => {
     e.preventDefault();
 
-    const author = inputValue;
-    const text = textareaValue;
-
-    setReview({ author, text });
+    setContactBook({ contacts, name });
   };
 
   return (
     <>
-      <h2>Dodaj Recenzje</h2>
+      <h2>Add phone number</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <div>
-            <label htmlFor="name">Autor</label>
+            <label htmlFor="name">Name</label>
           </div>
           <input
             type="text"
-            value={inputValue}
+            value={name}
             name="name"
             pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            title="qweqwe"
             required
             onChange={e => {
-              setInputValue(e.target.value);
+              setContactBook(e.target.value);
             }}
             id="name"
           />
         </div>
         <div>
           <div>
-            <label htmlFor="tel">Tekst</label>
+            <label htmlFor="tel">Phone number</label>
           </div>
 
           <input
             type="tel"
             name="name"
             id="tel"
-            value={textareaValue}
+            value={contacts}
             pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            title="qwe"
             required
             onChange={e => {
-              setTextareaValue(e.target.value);
+              setContactBook(e.target.value);
             }}
           />
         </div>
-        <button
-          type="submit"
-          disabled={inputValue === '' || textareaValue === ''}
-        >
+        <button type="submit" disabled={name === '' || contacts === ''}>
           Dodaj
         </button>
       </form>
-      {review && (
+      {name && (
         <ul>
           <h1>Książka tele:</h1>
           <li>
             <p>
-              <strong>{review.author}</strong>
+              <strong>
+                {name}: {contacts}
+              </strong>
             </p>
-            <p>{review.text}</p>
           </li>
         </ul>
       )}
