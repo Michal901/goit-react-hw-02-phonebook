167849dbff8154f8d73e9c4d9c8602b286cf42de
Michal901 <momot1122@wp.pl>
2024-04-14 22:30:37 +0200

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 const Filter = ({ filterValue, setFilterValue }) => {
   return (
     <div>
-      <label htmlFor="filter">Filter by Name:</label>
+      <div>
+        <label htmlFor="filter">Find contact by name:</label>
+      </div>
       <input
         type="text"
         id="filter"
