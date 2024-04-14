4799b813de8c1824d672e2c03482974ff04b3582
Michal901 <momot1122@wp.pl>
2024-04-14 18:12:05 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,11 @@ import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 
 export const App = () => {
-  const [contacts, setContacts] = useState({
-    contacts: [],
-    filter: '',
-  });
-
-  const addContact = newContact => {
-    setContacts(prevState => ({
-      ...prevState,
-      contacts: [...prevState.contacts, newContact],
-    }));
-  };
-
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm addContact={addContact} />
-      <ContactList contacts={contacts.contacts} />
+      <ContactForm />
+      <ContactList />
     </div>
   );
 };
